09cbd92d20f59fbf4c1e71e90fce3934cca7ac09
x00140684 <32486270+x00140684@users.noreply.github.com>
2018-12-12 14:40:34 +0000

    Update Header.js

--- lab7-news-search-master/components/Header.js
+++ lab7-news-search-master/components/Header.js
@@ -1,6 +1,7 @@
 const Header = () => (
     <div className= "header">
         <h2 className="title">World Wide News Network</h2>
+         <a href="https://newsapi.org">Powered by News API</a>
         <style jsx>{`
     .header{
         background-color: #a5a5a5;
